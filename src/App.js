b087fcb66b8ba7d12d8a4fe59ab619e01c38fd9a
nkhlrgh <43105834+nkhlrgh@users.noreply.github.com>
2023-03-16 12:58:01 +0530

    feature: add circle or cross alternatively on click of sqaure

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import { Cell } from "./components/Cell";
 
 const App = () => {
     const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+    const [go, setGo] = useState("circle");
+    const [winningMessage, setWinningMessage] = useState("");
+    const message = " it is now " + go + "'s go. ";
+
     return (
         <div className="app">
             <div className="gameboard">
                 {cells.map((cell, index) => (
-                    <Cell key={index} id={index} cell={cell} />
+                    <Cell
+                        key={index}
+                        id={index}
+                        cell={cell}
+                        setCells={setCells}
+                        go={go}
+                        setGo={setGo}
+                    />
                 ))}
             </div>
-            <p></p>
+            <p>{message}</p>
         </div>
     );
 };
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,4 +1,25 @@
-export const Cell = ({ id, cell }) => {
-    console.log(cell);
-    return <div className="square" id={id}></div>;
+export const Cell = ({ id, cell, setCells, go, setGo }) => {
+    const clickHandler = (e) => {
+        const taken =
+            e.target.firstChild.classList.contains("circle") ||
+            e.target.firstChild.classList.contains("cross");
+
+        if (!taken) {
+            if (go === "circle") {
+                e.target.firstChild.classList.add("circle");
+                setGo("cross");
+            }
+
+            if (go === "cross") {
+                e.target.firstChild.classList.add("cross");
+                setGo("circle");
+            }
+        }
+    };
+
+    return (
+        <div className="square" id={id} onClick={clickHandler}>
+            <div className={cell}></div>
+        </div>
+    );
 };
